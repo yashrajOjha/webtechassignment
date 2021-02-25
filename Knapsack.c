9e4844a8f23cd6cfeaa1a37f7c2c3a3d33468b11
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2021-02-24 22:34:03 -0800

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -1,10 +1,32 @@
 #include<stdio.h>
 
 void knapsack(int n, float weight[], float profit[], float capacity) {
-   float x[20], tp = 0;
-   int i, j, u;
-   u = capacity;
+    int ratio[n],temp;
+    float x[20], tp = 0;
+    int i, j, u;
+    u = capacity;
 
+   for (i = 0; i < n; i++) {
+      ratio[i] = profit[i] / weight[i];
+   }
+
+   for (i = 0; i < n; i++) {
+      for (j = i + 1; j < n; j++) {
+         if (ratio[i] < ratio[j]) {
+            temp = ratio[j];
+            ratio[j] = ratio[i];
+            ratio[i] = temp;
+
+            temp = weight[j];
+            weight[j] = weight[i];
+            weight[i] = temp;
+
+            temp = profit[j];
+            profit[j] = profit[i];
+            profit[i] = temp;
+         }
+      }
+   }
    for (i = 0; i < n; i++)
       x[i] = 0.0;
 
@@ -34,8 +56,6 @@ void knapsack(int n, float weight[], float profit[], float capacity) {
 int main() {
    float weight[20], profit[20], capacity;
    int num, i, j;
-   float ratio[20], temp;
-
    printf("\nEnter the no. of objects:- ");
    scanf("%d", &num);
 
@@ -44,31 +64,8 @@ int main() {
       scanf("%f %f", &weight[i], &profit[i]);
    }
 
-   printf("\nEnter the capacityacity of knapsack:- ");
+   printf("\nEnter the capacity of knapsack:- ");
    scanf("%f", &capacity);
-
-   for (i = 0; i < num; i++) {
-      ratio[i] = profit[i] / weight[i];
-   }
-
-   for (i = 0; i < num; i++) {
-      for (j = i + 1; j < num; j++) {
-         if (ratio[i] < ratio[j]) {
-            temp = ratio[j];
-            ratio[j] = ratio[i];
-            ratio[i] = temp;
-
-            temp = weight[j];
-            weight[j] = weight[i];
-            weight[i] = temp;
-
-            temp = profit[j];
-            profit[j] = profit[i];
-            profit[i] = temp;
-         }
-      }
-   }
-
    knapsack(num, weight, profit, capacity);
    return(0);
 }
